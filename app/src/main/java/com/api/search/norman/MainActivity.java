ffed1dcb8b284c52fcbd67f7b28942f50343fe72
Normansyah Putra <normansyah.putra@tokopedia.com>
2015-10-06 13:58:15 +0700

    1. add picasso ( and permission ) 2. endless scrolling for recyclerview 3. layout structure #1 attempt

--- app/src/main/java/com/api/search/norman/MainActivity.java
+++ app/src/main/java/com/api/search/norman/MainActivity.java
@@ -3,6 +3,7 @@ package com.api.search.norman;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -18,6 +19,8 @@ import com.api.search.norman.utils.JsonUtils;
 import com.nineoldandroids.view.ViewPropertyAnimator;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity implements Constant.JsonUtilsConstant{
 
@@ -32,67 +35,139 @@ public class MainActivity extends Activity implements Constant.JsonUtilsConstant
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Object[] rets = null;
+        Object[] returnJson = null;
         try {
-            rets = JsonUtils.getJsonDataFromAssets(this);
-            Log.d(Constant.LOG, rets.length+"");
+            returnJson = JsonUtils.getJsonDataFromAssets(this);
+            Log.d(Constant.LOG, returnJson.length+"");
 
             // gunakan rets[1] sebagai data
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // translation value
         mFlexibleSpaceOffset = getResources().getDimensionPixelSize(R.dimen.header_height);
 
         mHeader = findViewById(R.id.view_header);
+        View paddingView = setupHeaderView();
+        setupRecyclerView(returnJson);
+
+        ((InnerDataAdapter)mAdapter).addHeaderView(paddingView);
+    }
+
+    @NonNull
+    private View setupHeaderView() {
         View paddingView = new View(this);
         AbsListView.LayoutParams params = new AbsListView.LayoutParams(
                 AbsListView.LayoutParams.MATCH_PARENT, mFlexibleSpaceOffset
         );
         paddingView.setLayoutParams(params);
         paddingView.setBackgroundColor(Color.WHITE);
+        return paddingView;
+    }
+
+    private void setupRecyclerView(Object[] returnJson) {
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
+//        mRecyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         // specify an adapter (see also next example)
-        mAdapter = new InnerDataAdapter((Data)rets[DATA]);
-        ((InnerDataAdapter)mAdapter).addHeaderView(paddingView);
+
+        final Data data = (Data) returnJson[DATA];
+        Data newData = new Data();
+        List<Data.InnerData> firstHalf = new ArrayList<>();
+        for(int i=0;i<data.getDatas().size();i++){
+            firstHalf.add(data.getDatas().get(i));
+        }
+        newData.setDatas(firstHalf);
+        mAdapter = new InnerDataAdapter(newData);
         mRecyclerView.setAdapter(mAdapter);
-
         mRecyclerView.setOnScrollListener(
-                new RecyclerView.OnScrollListener() {
-                    boolean isIdle;
-                    int mScrollY;
-
+                new EndlessRecyclerOnScrollListener((LinearLayoutManager)mLayoutManager) {
                     @Override
-                    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                        super.onScrollStateChanged(recyclerView, newState);
-                        isIdle = newState == RecyclerView.SCROLL_STATE_IDLE;
-                        if (isIdle) {
-                            mScrollY = 0;
+                    public void onLoadMore(int current_page) {
+                        Log.d("MNORMANSYAH", "masuk load morenya");
+                        List<Data.InnerData> lastHalf = new ArrayList<>();
+                        for(int i=data.getDatas().size()/2;i<data.getDatas().size();i++){
+                            lastHalf.add(data.getDatas().get(i));
                         }
-                    }
-
-                    @Override
-                    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                        super.onScrolled(recyclerView, dx, dy);
-                        mScrollY += dy;
-                        // show or hide header view
-                        if (mScrollY > 12) {
-                            hideHeader();
-                        } else {
-                            showHeader();
-                        }
+                        Data newData = new Data(lastHalf);
+                        ((InnerDataAdapter)mAdapter).addNewData(newData);
                     }
                 }
         );
+    }
+
+    abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener{
+//        public static final String TAG = EndlessRecyclerOnScrollListener.class.getSimpleName();
+
+        private int previousTotal = 0; // The total number of items in the dataset after the last load
+        private boolean loading = true; // True if we are still waiting for the last set of data to load.
+        private int visibleThreshold = 5; // The minimum amount of items to have below your current scroll position before loading more.
+        int firstVisibleItem, visibleItemCount, totalItemCount;
+
+        private int current_page = 1;
+
+        private LinearLayoutManager mLinearLayoutManager;
+
+        boolean isIdle;
+        int mScrollY;
+
+        public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+            this.mLinearLayoutManager = linearLayoutManager;
+        }
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+
+            mScrollY += dy;
+            // show or hide header view
+            if (mScrollY > 12) {
+                hideHeader();
+            } else {
+                showHeader();
+            }
+
+            visibleItemCount = recyclerView.getChildCount();
+            totalItemCount = mLinearLayoutManager.getItemCount();
+            firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+            if (loading) {
+                if (totalItemCount > previousTotal) {
+                    loading = false;
+                    previousTotal = totalItemCount;
+                }
+            }
+            if (!loading && (totalItemCount - visibleItemCount)
+                    <= (firstVisibleItem + visibleThreshold)) {
+                // End has been reached
+
+                // Do something
+                current_page++;
+
+                onLoadMore(current_page);
+
+                loading = true;
+            }
+        }
+
+        @Override
+        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+            isIdle = newState == RecyclerView.SCROLL_STATE_IDLE;
+            if (isIdle) {
+                mScrollY = 0;
+            }
+        }
+
+        public abstract void onLoadMore(int current_page);
     }
 
     private void showHeader() {
--- app/src/main/java/com/api/search/norman/adapters/InnerDataAdapter.java
+++ app/src/main/java/com/api/search/norman/adapters/InnerDataAdapter.java
@@ -4,12 +4,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.api.search.norman.R;
 import com.api.search.norman.models.Data;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
+
+import java.util.List;
 
 /**
  * Created by m.normansyah on 02/10/2015.
@@ -28,14 +32,17 @@ public class InnerDataAdapter extends RecyclerView.Adapter<InnerDataAdapter.View
     private final int TYPE_HEADER = 0;
     private final int TYPE_CHILD = 1;
     private View mView;
+    private Picasso picasso;
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         if (mView != null && i == TYPE_HEADER) {
             return new ViewHolder(mView);
         }else {
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_test, viewGroup, false);
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listview_prod_view, viewGroup, false);
             ViewHolder vh = new ViewHolder(v);
+            if(picasso == null)
+                picasso = picasso.with(viewGroup.getContext());
             return vh;
         }
     }
@@ -46,7 +53,15 @@ public class InnerDataAdapter extends RecyclerView.Adapter<InnerDataAdapter.View
         if (mView != null) {
             i = i - 1;
         }
-        viewHolder.mTextView.setText(data.getDatas().get(i).getId()+" "+data.getDatas().get(i).getImageUri());
+        // set data here
+        if(data.getDatas().get(i).getName()!=null)
+            viewHolder.prodName.setText(viewHolder.prodName.getText()+data.getDatas().get(i).getName());
+        if(data.getDatas().get(i).getShop().getName()!=null)
+            viewHolder.prodShopName.setText(viewHolder.prodShopName.getText()+" : "+data.getDatas().get(i).getShop().getName());
+        if(data.getDatas().get(i).getPrice()!=null)
+            viewHolder.prodPrice.setText(viewHolder.prodPrice.getText()+" : "+data.getDatas().get(i).getPrice());
+        Picasso.with(viewHolder.prodImg.getContext()).load(data.getDatas().get(i).getImageUri()).error(R.mipmap.my_avatar).into(viewHolder.prodImg);
+
     }
 
     @Override
@@ -63,11 +78,26 @@ public class InnerDataAdapter extends RecyclerView.Adapter<InnerDataAdapter.View
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
+        public TextView prodName;
+        public ImageView prodImg;
+        public TextView prodShopName;
+        public TextView prodPrice;
+        public TextView boughtReviewView;
+
         public ViewHolder(View v) {
             super(v);
-            mTextView = (TextView)v.findViewById(R.id.hitAllhere);
+            prodName = (TextView)v.findViewById(R.id.prod_name);
+            prodImg = (ImageView)v.findViewById(R.id.prod_img);
+            prodShopName = (TextView)v.findViewById(R.id.prod_shop_name);
+            prodPrice = (TextView)v.findViewById(R.id.prod_price);
+            boughtReviewView = (TextView)v.findViewById(R.id.bought_review_view);
         }
+    }
+
+    public void addNewData(Data newData){
+        for(int i=0;i<newData.getDatas().size();i++){
+            data.addData(newData.getDatas().get(i));
+        }
+        notifyDataSetChanged();
     }
 }
